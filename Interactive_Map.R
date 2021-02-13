bcfba8fd23930d8bd02dcfe1736e0eefaf253967
Bathyscapher <korn@cumulonimbus.at>
2021-02-13 14:07:02 +0100

    Read and process EPI data from function

--- Interactive_Map.R
+++ Interactive_Map.R
@@ -1,26 +1,37 @@
-# library(dplyr)
-# library(ggplot2)
-# library(rjson)
-# library(jsonlite)
+###############################################################################
+###############################################################################
+# Visualization of the Environmental Performance Index (EPI)
+
+
+library("reshape2")
+library("ggplot2")
+theme_set(theme_bw(base_size = 12) +
+            theme(rect = element_rect(fill = "transparent")))
 library("leaflet")
-# library(RCurl)
-# library("tmap")
-# library("sf")
-# library("mapview")
-# library("spData")
-# library(shiny)
 library("rgdal")
-library("RColorBrewer")
 library("htmlwidgets")
 
 
-# rm(list = ls())
+## Clear workspace, set working directory
+rm(list = ls())
+setwd("~/epi")
 
 
+## Load utility functions
+source("UtilityFunctions.R")
 
-# Choropleth map with leaflet
-## Download the shapefile with world borders. Note: overwrites existing files!
-if(!file.exists("TM_WORLD_BORDERS-0.3.shp")){
+
+## Import EPI data
+epi.list <- importEPI()
+
+
+list2env(epi.list, .GlobalEnv)
+rm(epi.list)
+
+
+## Choropleth map with leaflet
+### Download the shapefile with world borders. Note: overwrites existing files!
+if(!file.exists("2020/TM_WORLD_BORDERS-0.3.shp")){
   print("File does not exist, downloading it now:")
   download.file("thematicmapping.org/downloads/TM_WORLD_BORDERS-0.3.zip",
                 destfile="world_shape_file.zip")
@@ -29,117 +40,135 @@ if(!file.exists("TM_WORLD_BORDERS-0.3.shp")){
 }
 
 
-## Read the shape file
-world_spdf <- readOGR(dsn = ".", layer = "TM_WORLD_BORDERS-0.3", verbose = TRUE)
-class(world_spdf)
-summary(world_spdf)
+### Read the shape file
+world.spdf <- readOGR(dsn = "2020", layer = "TM_WORLD_BORDERS-0.3",
+                      verbose = TRUE)
+class(world.spdf)
+summary(world.spdf)
 
 
-## Collate countries and their names (as soon as it comes to map the world,
-## geopolitics come to the fore...)
-length(world_spdf$NAME) # many more countries in the shp file
+### Collate countries and their names (as soon as it comes to map the world,
+### geopolitics come to the fore...)
+length(world.spdf$NAME) # many more countries in the shp file
 length(epi$country)
 
-sort(world_spdf$NAME)
+sort(world.spdf$NAME)
 sort(epi$country)
 
-sort(setdiff(world_spdf$NAME, epi$country)) # differences due to naming and numbers
-sort(setdiff(epi$country, world_spdf$NAME))
+sort(setdiff(world.spdf$NAME, epi$country)) # differences due to naming and numbers
+sort(setdiff(epi$country, world.spdf$NAME))
 
 
-#### Rename countries with their more up-to-date or shorter name
-epi$country[epi$country == "Cabo Verde"] <- "Cape Verde"
-epi$country[epi$country == "Dem. Rep. Congo"] <-
-  "Democratic Republic of the Congo"
-epi$country[epi$country == "Eswatini"] <- "Swaziland"
-world_spdf$NAME[world_spdf$NAME == "Iran (Islamic Republic of)"] <- "Iran"
-world_spdf$NAME[world_spdf$NAME == "Lao People's Democratic Republic"] <- "Laos"
-world_spdf$NAME[world_spdf$NAME == "Micronesia, Federated States of"] <-
-  "Micronesia"
-world_spdf$NAME[world_spdf$NAME == "Republic of Moldova"] <- "Moldova"
-world_spdf$NAME[world_spdf$NAME == "Burma"] <- "Myanmar"
-world_spdf$NAME[world_spdf$NAME ==
-                  "The former Yugoslav Republic of Macedonia"] <-
-  "North Macedonia"
-world_spdf$NAME[world_spdf$NAME == "Congo"] <- "Republic of Congo"
-world_spdf$NAME[world_spdf$NAME == "Korea, Republic of"] <-
-  "South Korea"
-world_spdf$NAME[world_spdf$NAME == "United Republic of Tanzania"] <- "Tanzania"
-world_spdf$NAME[world_spdf$NAME == "United States"] <-
-  "United States of America"
+epi.world.spdf <- collateCountryNames(epi, world.spdf)
+list2env(epi.world.spdf, .GlobalEnv)
+rm(epi.world.spdf)
 
 
-
-#### Control if all EPI countries are present in the world map
-sort(setdiff(epi$country, world_spdf$NAME))
-
-
-#### Countries without an EPI are miniature states, islands or states currently
-#### in an armed conflict. For now, keep it as is. Later possibly merge islands
-#### into their mainlands...
-sort(setdiff(world_spdf$NAME, epi$country))
+##### Control if all EPI countries are present in the world map
+sort(setdiff(epi$country, world.spdf$NAME))
 
 
-## Add EPI and regions to shp file
-world_spdf <- merge(world_spdf, epi[, c(1, 4, 50)], by.x = "NAME", by.y = "country")
-names(world_spdf)
+##### Countries without an EPI are miniature states, islands or states currently
+##### in an armed conflict. For now, keep it as is. Later possibly merge islands
+##### into their mainlands...
+sort(setdiff(world.spdf$NAME, epi$country))
 
 
-## Enhance readability of population numbers
-world_spdf@data$POP2005[which(world_spdf@data$POP2005 == 0)] <- NA
-world_spdf@data$POP2005 <- as.integer(world_spdf@data$POP2005) / 1000000 %>%
+### Add EPI and regions to shp file
+world.spdf <- merge(world.spdf, epi[, c(1, 4, 50)], by.x = "NAME",
+                    by.y = "country")
+names(world.spdf)
+
+
+## Encode population numbers as millions
+world.spdf@data$POP2005[which(world.spdf@data$POP2005 == 0)] <- NA
+world.spdf@data$POP2005 <- as.integer(world.spdf@data$POP2005) / 1000000 %>%
   round(2)
 
 
 # Create a color palette for the map
 ## EPI color scheme
-epi.colors <- colorNumeric(palette = "viridis", domain = world_spdf@data$EPI.new,
-                          na.color = "gray", reverse = TRUE)
-# epi.colors(c(45, 43))
+epi.cols <- colorNumeric(palette = "viridis", domain = world.spdf@data$EPI.new,
+                         na.color = "gray", reverse = TRUE)
 
 
 ## Region color scheme
-region.colors <- colorFactor(palette = "plasma",
-                             domain = world_spdf@data$region, na.color = "gray")
+region.cols <- colorFactor(palette = "plasma", domain = world.spdf@data$region,
+                           na.color = "gray")
 
 
-# Prepare the text for tooltips
-tooltips <- paste("Country:", world_spdf@data$NAME, "<br/>",
-                  "Region:", world_spdf@data$region, "<br/r>",
-                  # "Area:", world_spdf@data$AREA, "<br/>",
-                  "Population:", round(world_spdf@data$POP2005, 2), "M", "<br/>",
-                  "EPI:", world_spdf@data$EPI.new) %>%
+## Prepare the text for tooltips
+tooltips <- paste("Country:", world.spdf@data$NAME, "<br/>",
+                  "Region:", world.spdf@data$region, "<br/r>",
+                  # "Area:", world.spdf@data$AREA, "<br/>",
+                  "Population:", round(world.spdf@data$POP2005, 2), "M",
+                  "<br/>",
+                  "EPI:", world.spdf@data$EPI.new) %>%
   lapply(htmltools::HTML)
 
 
-# The map
-m <- leaflet(world_spdf) %>%
+## Choropleth map
+cp.map <- leaflet(world.spdf) %>%
   addTiles() %>%
   setView(lat = 20, lng = 0, zoom = 2.5) %>%
-  addPolygons(fillColor = ~ epi.colors(EPI.new), stroke = TRUE, group = "EPI",
-              fillOpacity = 0.9, color = "white", weight = 0.5, label = tooltips,
+  addPolygons(fillColor = ~ epi.cols(EPI.new), stroke = TRUE, group = "EPI",
+              fillOpacity = 0.9, color = "white", weight = 0.5,
+              label = tooltips,
               labelOptions = labelOptions(style = list("font-weight" = "normal",
                                                        padding = "3px 8px"),
                                           textsize = "13px",
                                           direction = "auto")) %>%
-  addPolygons(fillColor = ~ region.colors(region), stroke = TRUE, group = "Region",
-              fillOpacity = 0.5, color = "white", weight = 0.5, label = tooltips,
+  addPolygons(fillColor = ~ region.cols(region), stroke = TRUE,
+              group = "Region", fillOpacity = 0.5, color = "white",
+              weight = 0.5, label = tooltips,
               labelOptions = labelOptions(style = list("font-weight" = "normal",
                                                        padding = "3px 8px"),
                                           textsize = "13px",
                                           direction = "auto")) %>%
-  addLegend(pal = epi.colors, values = ~ EPI.new, opacity = 0.9,
+  addLegend(pal = epi.cols, values = ~ EPI.new, opacity = 0.9,
             title = "EPI", position = "topleft", group = "EPI") %>%
-  addLegend(pal = region.colors, values = ~ region, opacity = 0.5,
+  addLegend(pal = region.cols, values = ~ region, opacity = 0.5,
             title = "", position = "topleft", group = "Region") %>%
   addLayersControl(overlayGroups = c("EPI", "Region"),
                    options = layersControlOptions(collapsed = FALSE)) %>%
   hideGroup("Region")
-m
+cp.map
 
 
 # Export to html
-saveWidget(m, file = "../index.html")
+saveWidget(cp.map, file = "index.html")
+
+
+## Explore
+### Colorblind color scheme
+cb.cols <- c("#56B4E9", "#cc79a7", "#D55E00", "#0072b2", "#E69F00", "#999999",
+             "#F0E442", "#009E73")
+
+
+### Boxplots sorted by median
+ggplot(epi.long[epi.long$Type == "Indicator", ],
+       aes(x = reorder(country, EPI.new.value, FUN = median, na.rm = TRUE),
+           y = EPI.new.value, fill = region)) +
+  geom_boxplot(outlier.shape = 21, outlier.size = 1) +
+  scale_fill_manual(values = cb.cols) +
+  theme(legend.position = "top", legend.title = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.text = element_text(size = 5)) +
+  ylab("EPIs") +
+  xlab("")
+ggsave("tst.pdf", width = 20, height = 6.27, bg = "transparent")
+
+
+ggplot(epi.long[epi.long$Type == "EPI", ],
+       aes(x = reorder(region, EPI.new.value, FUN = median, na.rm = TRUE),
+           y = EPI.new.value, fill = region)) +
+  geom_boxplot(outlier.shape = 21, outlier.size = 1) +
+  scale_fill_manual(values = cb.cols) +
+  theme(legend.position = "top", legend.title = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ylab("EPI 2020") +
+  xlab("")
+ggsave("tst_region.pdf", width = 20, height = 6.27, bg = "transparent")
 
 
 ################################################################################
---
+++ UtilityFunctions.R
@@ -0,0 +1,129 @@
+################################################################################
+################################################################################
+# Data preparation: Environmental Performance Index (EPI)
+
+require("reshape2")
+
+
+# rm(list = ls())
+# setwd("~/epi")
+
+
+## Import EPI data
+importEPI <- function(){
+  # If necessary, download files to directory "2020"
+  if(!file.exists("2020/epi2020results20200604.csv") |
+     !file.exists("2020/epi2020indicatortla20200604.csv") |
+     !file.exists("2020/epi2020countryattributes20200604.csv") |
+     !file.exists("2020/epi2020variableattributes20200604.csv")) {
+    "Files absent, downloading them now:"
+    system("wget -P 2020 https://epi.yale.edu/downloads/epi2020results20200604.csv https://epi.yale.edu/downloads/epi2020indicatortla20200604.csv https://epi.yale.edu/downloads/epi2020countryattributes20200604.csv https://epi.yale.edu/downloads/epi2020variableattributes20200604.csv")
+  }
+
+
+  # Read all csv files
+  epi <- lapply(list.files(pattern = glob2rx("epi2020*.csv"), recursive = TRUE),
+                read.table, sep = ",", header = TRUE, quote = "")
+  # lapply(epi, head)
+  # lapply(epi, str)
+  # class(epi)
+  # epi
+
+
+  # Name the list by retrieving the file names and extract the informative part
+  names(epi) <- gsub("\\d{8}.csv", "",
+                     gsub("2020/epi2020", "",
+                          list.files(pattern = glob2rx("epi2020*.csv"),
+                                     recursive = TRUE)))
+  # names(epi)
+
+
+  # Release data.frames from list into global environment
+  list2env(epi, .GlobalEnv)
+  # rm(epi)
+
+
+  # Rename to more "speaking" names
+  epi <- results # EPIs by country
+  tla <- indicatortla # description of EPIs = three-letter-abbreviation
+  countries <- countryattributes
+
+  rm(results, indicatortla, countryattributes, variableattributes,
+     envir = .GlobalEnv)
+
+
+  # Add regions
+  epi <- merge(epi, countries[, 3:4], by = "country")
+  # str(epi)
+
+
+  # Subset by selecting columns which match "new" (for the 2020 EPI values) and
+  # a negative lookbehind to exclude columns containing the new ranks. Also keep
+  # the columns "code", "iso" and "country" (i.e. all that contain only letters)
+  epi <- epi[, (colnames(epi) %in% grep("(?<!rnk).new|^[^.]*$", names(epi),
+                                        value = TRUE, perl = TRUE)),
+             drop = FALSE]
+  # names(epi)
+
+
+  # Convert into long form
+  epi.long <- melt(epi, id.vars = c("code", "iso", "country", "region"),
+                   value.name = "EPI.new.value", variable.name = "EPI.new")
+  # str(epi.long)
+  summary(epi.long$EPI.new.value)
+
+
+  # Convert to factor
+  epi.long$country <- as.factor(epi.long$country)
+  epi.long$region <- as.factor(epi.long$region)
+
+
+  ## Remove ".new" from EPIs
+  epi.long$EPI.new <- as.factor(substr(epi.long$EPI.new, 1, 3))
+  str(epi.long)
+
+
+  ## Add EPI levels
+  epi.long <- merge(epi.long, tla[, 1:3],
+                    by.x = "EPI.new", by.y = "Abbreviation")
+
+  epi.list <- list(epi = epi, epi.long = epi.long)
+  return(epi.list)
+}
+
+
+## Rename countries with their more up-to-date or shorter name
+collateCountryNames <- function(epi, world.spdf) {
+  epi$country[epi$country == "Cabo Verde"] <- "Cape Verde"
+  epi$country[epi$country == "Dem. Rep. Congo"] <-
+    "Democratic Republic of the Congo"
+  epi$country[epi$country == "Eswatini"] <- "Swaziland"
+  world.spdf$NAME[world.spdf$NAME == "Iran (Islamic Republic of)"] <- "Iran"
+  world.spdf$NAME[world.spdf$NAME == "Lao People's Democratic Republic"] <-
+    "Laos"
+  world.spdf$NAME[world.spdf$NAME == "Micronesia, Federated States of"] <-
+    "Micronesia"
+  world.spdf$NAME[world.spdf$NAME == "Republic of Moldova"] <- "Moldova"
+  world.spdf$NAME[world.spdf$NAME == "Burma"] <- "Myanmar"
+  world.spdf$NAME[world.spdf$NAME ==
+                    "The former Yugoslav Republic of Macedonia"] <-
+    "North Macedonia"
+  world.spdf$NAME[world.spdf$NAME == "Congo"] <- "Republic of Congo"
+  world.spdf$NAME[world.spdf$NAME == "Korea, Republic of"] <-
+    "South Korea"
+  world.spdf$NAME[world.spdf$NAME ==
+                    "Korea, Democratic People's Republic of"] <- "North Korea"
+  world.spdf$NAME[world.spdf$NAME == "United Republic of Tanzania"] <-
+    "Tanzania"
+  world.spdf$NAME[world.spdf$NAME == "United States"] <-
+    "United States of America"
+
+  epi.world.spdf <- list(epi = epi, world.spdf = world.spdf)
+  return(epi.world.spdf)
+}
+
+
+
+
+################################################################################
+################################################################################
